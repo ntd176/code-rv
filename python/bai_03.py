5a5bf1a05ce74d365e6e4ca3e9214cb0d65c2aab
ntd176 <tiendai17062003@gmail.com>
2025-09-21 16:01:05 +0700

    upload_py_1

---
+++ python/bai_03.py
@@ -0,0 +1,30 @@
+# chú thích từng dòng
+
+"""
+chú thích nhiều dòng
+"""
+"""
+Create on Sun Sep 21 2025 15:40
+@author: ntdai
+"""
+# nhap du lieu
+a = input("nhap vao a:")
+
+# xu ly
+a = int(a) + 2
+
+# xuat du lieu
+#a=1000
+print(a)
+
+"""
+{
+    KHÔNG CÓ
+}
+"""
+if a > 0:
+    print(a, "là số dương.")
+"""
+Sử dụng Tab -> thụt lề sang phải
+        Shift + Tab -> Thụt lề sang trái
+"""
