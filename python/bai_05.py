de024cf5063db044e7aa37f9f8fd9c64e7d5b539
ntd176 <tiendai17062003@gmail.com>
2025-09-24 21:06:14 +0700

    upload_py

---
+++ python/bai_05.py
@@ -0,0 +1,17 @@
+﻿'''
+Create Wed Sep 24 2025 15:00
+@author: ntdai
+
+#05 NHẬP DỮ LIỆU BẰNG input
+Cú pháp:
+    input(prompt)
+        prompt: chuỗi thông báo
+'''
+input("Nhập vào một con số: ")
+
+x = input("Nhập vào x: ")
+print("x = {0}" .format(x))
+
+ho = input("Nhập vào họ: ");
+ten = input("Nhập vào ten: ");
+print("Xin chào", ho, ten)
