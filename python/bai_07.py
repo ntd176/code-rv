49892386735b11bca9a0bafd47d9b690cf1f9d3f
ntd176 <tiendai17062003@gmail.com>
2025-09-26 15:09:47 +0700

    upload_py

---
+++ python/bai_07.py
@@ -0,0 +1,34 @@
+﻿'''
+Create Fri Sep 26 2025 15:00
+@author: ntdai
+
+#07 KIỂU DỮ LIỆU
+    Python là một ngôn ngữ thông dịch (không yêu cầu biên dịch), không bắt buộc phải khai báo kiểu dữ liệu của biến
+    Trình thông dịch tự đoán kiểu dữ liệu
+    Cách kiểm tra dữ liệu của biến:
+        type(tên_biến)
+'''
+x = 1
+print(type(x))
+
+x = 1.0
+print(type(x))
+
+x = 1 + 2j
+print(type(x))
+
+x = True
+print(type(x))
+
+x = "abc"
+print(type(x))
+
+x = None
+print(type(x))
+
+e = 2.5
+pi = 3.14
+text = "Hello Tien Dai"
+print("Kiểu dữ liệu của biến e:", type(e),
+      ", Kiểu dữ liệu của biến pi:", type(pi),
+      ", Kiểu dữ liệu của biến text:", type(text))
---
+++ python/bai_08.py
@@ -0,0 +1,22 @@
+﻿'''
+Create Fri Sep 26 2025 15:30
+@author: ntdai
+
+#07 ÉP KIỂU DỮ LIỆU
+    ten_kieu_du_lieu(biến)
+        int(), float(), str()...
+'''
+# Kiểu dữ liệu ngầm hiểu
+a = 5
+b = 2.0
+c = a / b
+print(c)
+print("Kiểu dữ liệu của a: ", type(a))
+print("Kiểu dữ liệu của b: ", type(b))
+print("Kiểu dữ liệu của c: ", type(c))
+#-------------------------------------
+# Ép kiểu dữ liệu
+n = 100
+m = "200"
+print(str(n)+m)
+print(n+int(m))
