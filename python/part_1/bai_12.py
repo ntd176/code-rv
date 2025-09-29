e6345617d829ef5ba0cf7a57a99046b7ba9fba36
ntd176 <tiendai17062003@gmail.com>
2025-09-29 15:13:18 +0700

    upload_py

---
+++ python/part_1/bai_12.py
@@ -0,0 +1,20 @@
+﻿'''
+Create Mon Sep 29 2025 14:22
+@author: ntdai
+
+#12 THƯ VIỆN TOÁN HỌC MATH
+'''
+import math
+
+x = float(input("x: "))
+
+# Trị tuyệt đối
+print("|x| = ", math.fabs(x))
+# Căn bậc hai
+print("sqrt(x) = ", math.sqrt(x))
+# Làm tròn lên
+print("x_up = ", math.ceil(x))
+# Làm tròn xuống
+print("x_down = ", math.floor(x))
+# Số pi
+print("pi = ", math.pi)
