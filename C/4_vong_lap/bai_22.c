d5a1a2cee851f18f5fbc07bcd6a3bbddf42b55a1
ntd176 <tiendai17062003@gmail.com>
2025-10-08 21:55:33 +0700

    upload_c

--- C/4_vong_lap/bai_22.c
+++ C/4_vong_lap/bai_22.c
@@ -4,7 +4,8 @@ Create on Tue Oct 07 2025 15:12
 
 #23 TIM UOC SO CHUNG LON NHAT
 	Cach 1: giai thuat Euclid
-			
+	Cach 2: su dung phep tru
+	Cach 3: su dung phep chia du
 */
 
 // cach 1
@@ -24,5 +25,47 @@ int main(){
 		b = r;
 	}
 	printf("UCLN = %d", a);
+}
+
+// cach 2
+#include "stdio.h"
+int main(){
+	// nhap du lieu
+	int a, b;
+	printf("nhap a = ");
+	scanf("%d", &a);
+	printf("nhap b = ");
+	scanf("%d", &b);
+	// xu ly
+	if(a==0 || b==0){
+		printf("UCLN = %d", a+b);
+	}while(a!=b){
+		if(a>b){
+			a=a-b;	
+		}else{
+			b=b-a;
+		}
+	}
+	printf("UCLN = %d", a);
+}
+
+// cach 3
+#include "stdio.h"
+int main(){
+	// nhap du lieu
+	int a, b;
+	printf("nhap a = ");
+	scanf("%d", &a);
+	printf("nhap b = ");
+	scanf("%d", &b);
+	// xu ly
+	while(a*b != 0){
+		if(a>b){
+			a=a%b;
+		}else{
+			b=b%a;
+		}
+	}
+	printf("UCLN = %d", a+b);
 }
 
