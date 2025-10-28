de3fa8a4960c1c79700fad078c3cf1ae11541f84
ntd176 <tiendai17062003@gmail.com>
2025-10-28 14:33:26 +0700

    upload_c_bt_buoi_1

--- C/10_bai_tap/buoi_1/bai_11.c
+++ C/10_bai_tap/buoi_1/bai_11.c
@@ -94,8 +94,57 @@ int main(){
 	printf("nhap ky tu: ");
 	scanf("%c", &c);
 	//--------------
+	// Bang ma ASCII
+	// Chu hoa: 'A' -> 'Z': 65->90
+	// Chu thuong: 'a' -> 'z': 97->122
 	if (c>='A' && c<='Z'){
-		c = c + 32;
+		c = c + 32; // hoa sang thuong
+	}
+	printf("ky tu sau khi chuyen: %c", c);
+}
+/*-------------------------------
+#18 CHUYEN KY TU THUONG THANH HOA
+nhap vao ky tu c.
+Neu ky tu nhap la chu thuong thi in ra chu hoa tuong ung, nguoc lai thi khong thay doi ky tu ban dau
+*/
+#include "stdio.h"
+int main(){
+	char c;
+	printf("nhap ky tu: ");
+	scanf("%c", &c);
+	//--------------
+	// Bang ma ASCII
+	// Chu hoa: 'A' -> 'Z': 65->90
+	// Chu thuong: 'a' -> 'z': 97->122
+	if (c>='a' && c<='z'){
+		c = c - 32; // hoa sang thuong
+	}
+	printf("ky tu sau khi chuyen: %c", c);
+}
+/*-------------------------------
+#19 CHU CAI KE TIEP
+Nhap vao ky tu c.
+Neu ky tu nhap la chu cai thi in ra chu cai tuong ung ke tiep o dang chu thuong, ta coi chu cai ke tiep cua z la a.
+Neu ky tu nhap vao khong phai la chu cai thi xuat ra INVALID.
+*/
+#include "stdio.h"
+int main(){
+	char c;
+	printf("nhap ky tu: ");
+	scanf("%c", &c);
+	//--------------
+	// Bang ma ASCII
+	// Chu hoa: 'A' -> 'Z': 65->90
+	// Chu thuong: 'a' -> 'z': 97->122
+	if ((c>='a' && c<='z') || (c>='A' && c<='Z')){
+		// chuyen ve chu thuong
+		if (c>='A' && c<='Z')
+			c = c + 32;
+		// tim chu cai ke tiep
+		if (c=='z')
+			c = 'a';
+		else 
+			c = c + 1;
 	}
 	printf("ky tu sau khi chuyen: %c", c);
 }
