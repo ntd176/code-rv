dc0f2fc04a9427dbb429cfbb4b023d1f2c9dba19
ntd176 <tiendai17062003@gmail.com>
2025-11-03 15:04:30 +0700

    upload_c

---
+++ C/6_mang_1_chieu/bai_48.cpp
@@ -0,0 +1,79 @@
+/*
+Create on Mon Nov 3 2025 13:45
+@author: ntdai
+
+#49 KIEM TRA MANG B CO PHAI MANG CON (LIEN TIEP) CUA MANG A
+*/
+
+#include "stdio.h"
+
+// nhap mang
+void nhapMang(int x[], int &n, char tenMang){
+	do {
+		printf("nhap so luong phan tu (0<n<100): ", tenMang);
+		scanf("%d", &n);
+	} while (n<1 || n>100);
+	//---------------------
+	printf("nhap du lieu cho mang:\n");
+	for(int i=0; i<n; i++){
+		printf("%c[%d] = ", tenMang, i);
+		scanf("%d", &x[i]);
+	}
+}
+// xuat mang
+void xuatMang(int x[], int n, char tenMang){
+	printf("Mang %c: ", tenMang);
+	for(int i=0; i<n; i++){
+		printf("%d ", x[i]);
+	}
+	printf("\n");
+}
+// check B la mang con cua A khong ?
+int checkMangCon(int a[], int n1, int b[], int n2){
+	// n1, n2 lan luot la so phan tu mang A, B
+	if (n2>n1)
+		return 0;
+	for (int i=0; i<=n1-n2; i++){
+		// debug 
+		printf("so sanh tai %d: ", i);
+		int j;
+		for (int j=0; j<n2; j++){
+			printf("%d-%d ", a[i+j], b[j]);
+		}
+		printf("\n");
+		//-----------
+		for (j=0; j<n2; j++){
+			if (a[i+j] != b[j])
+				break; // chi can 1 phan tu khac la stop
+		}
+		if (j==n2)
+			return 1; // mang con
+	}
+	return 0; // khong phai mang con
+}
+
+int main(){
+	int a[100], b[100];
+	int n1, n2;
+	
+	nhapMang(a, n1, 'A');
+	xuatMang(a, n1, 'A');
+	nhapMang(b, n2, 'B');
+	xuatMang(b, n2, 'B');
+	
+	if (checkMangCon(a, n1, b, n2)){
+		printf("Mang con");
+	} else {
+		printf("Khong phai mang con");
+	}
+}
+/*
+vi du
+	a = [1, 2, 3, 4, 5, 6] 
+	b = [3, 4, 5]
+	=> mang con (lien tiep)
+	-----------
+	a = [1, 2, 3, 4, 5, 6]
+	b = [2, 4]
+	=> khong phai mang con (khong lien tiep)
+*/
