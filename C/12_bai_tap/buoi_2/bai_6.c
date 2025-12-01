0b6cd4024912bf5d1d897c4523c30510d79a63d0
ntd176 <tiendai17062003@gmail.com>
2025-12-01 20:11:45 +0700

    upload_c_bt_buoi_2_bai_6

--- C/12_bai_tap/buoi_2/bai_6.c
+++ C/12_bai_tap/buoi_2/bai_6.c
@@ -2,7 +2,25 @@
 	File		 : bai 6
 	Author		 : Tien Dai Nguyen
 	Init Time	 : 17:50
-	Modifile Time: 17:50
+	Modifile Time: 17:55
 	Date		 : 2025/11/30
-	Modified Date: 2025/11/30
+	Modified Date: 2025/12/01
 */
+
+#include "stdio.h"
+
+int main(){
+	long long n, a, b;
+	scanf("%lld %lld %lld", &n, &a, &b);
+	//----------------------------------
+	long long cost;
+	//----------------------------------
+	if(b<2*a){
+		cost = (n/2)*b + (n%2)*a;
+	} else {
+		cost = n*a;
+	}
+	printf("%lld", cost);
+}
+
+
